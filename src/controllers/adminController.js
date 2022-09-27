5d6b0e47e91b6f415c28cda7c823855bee4e80a5
Ana Santoandre <anasantoandre84@gmail.com>
2022-09-27 16:43:13 -0300

    validaciones y mensaje

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,7 +1,9 @@
 const db = require("../database/models");
 const Op = db.Sequelize.Op;
+const {validationResult} = require("express-validator");
 const fs = require("fs");
 const path = require("path");
+
 const controladorAdmin = {
     panel:(req,res)=>{
         res.render("panel");
@@ -16,22 +18,33 @@ const controladorAdmin = {
         });
     },
     guardarCreado:(req,res)=>{
-        db.Producto.create({
-            nombre:req.body.nombre,
-            modelo:req.body.modelo,
-            marca:req.body.marca,
-            descripcion:req.body.descripcion,
-            precio:req.body.precio,
-            foto:req.file.path,
-            destacado:req.body.destacado,
-            oferta:req.body.oferta,
-            codigo:req.body.codigo,
-            medidas:req.body.medidas,
-            idTamanios:req.body.tamanio,
-            idCategorias:req.body.categoria
-        }).then((resultado)=>{
-            res.render("creadoExitoso");
-        })
+        const errores = validationResult(req);
+        if(errores.isEmpty()){
+            db.Producto.create({
+                nombre:req.body.nombre,
+                modelo:req.body.modelo,
+                marca:req.body.marca,
+                descripcion:req.body.descripcion,
+                precio:req.body.precio,
+                foto:req.file.path,
+                destacado:req.body.destacado,
+                oferta:req.body.oferta,
+                codigo:req.body.codigo,
+                medidas:req.body.medidas,
+                idTamanios:req.body.tamanio,
+                idCategorias:req.body.categoria
+            }).then((resultado)=>{
+                res.render("creadoExitoso");
+            })
+        }
+        else{
+            const pedidoCategorias = db.Categorias.findAll();
+            const pedidoTamanios = db.Tamanios.findAll();
+            Promise.all([pedidoCategorias,pedidoTamanios])
+                .then(([categorias,tamanios])=>{
+                    res.render("crear",{errores:errores.mapped(),categorias,tamanios});
+                });
+        };
     },
     editar:(req,res)=>{
         const pedidoCategorias = db.Categorias.findAll();
