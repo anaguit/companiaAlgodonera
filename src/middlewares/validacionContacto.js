f70536382c7320f6f5bbf2708707bbd63629e95a
Ana Santoandre <anasantoandre84@gmail.com>
2022-10-31 14:37:21 -0300

    cambio campo contacto

--- src/middlewares/validacionContacto.js
+++ src/middlewares/validacionContacto.js
@@ -2,7 +2,7 @@ const {body} = require("express-validator");
 
 const validacionContacto = [
     body("nombre").notEmpty().withMessage("campo obligatorio"),
-    body("mail").notEmpty().isEmail().withMessage("ingrese un mail válido"),
+    body("mail").notEmpty().withMessage("campo obligatorio"),
     body("asunto").notEmpty().withMessage("indique un asunto"),
     body("mensaje").notEmpty().withMessage("campo obligatorio")
 ];
