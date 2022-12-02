81647079ce9e57bfebeeac49463ff27d7d9932be
Ana Santoandre <anasantoandre84@gmail.com>
2022-12-02 13:59:37 -0300

    agregue boton filtros para celu y el evento click

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,11 @@
 window.addEventListener("load",function(){
-    
+    let botonBuscadorcito = document.querySelector(".botonBuscadorcito")
+    botonBuscadorcito.addEventListener("click",function(){
+        let buscadorcito = document.querySelector(".buscadorcito")
+        buscadorcito.style.display = "none"
+
+        let buscador = document.querySelector(".buscador")
+        buscador.style.display = "flex"
+        buscador.style.flexDirection = "column"
+    })
 })
