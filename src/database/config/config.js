f68529251c1aed1e6ca7f06b7eb37eedffaa0a9c
Ana Santoandre <anasantoandre84@gmail.com>
2022-12-25 14:19:29 -0300

    configuré las variables de entorno para clever cloud

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,15 +1,16 @@
+require("dotenv").config();
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "companiaalgodonera",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": null,
-    "database": "database_test",
+    "database": "companiaalgodonera",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
