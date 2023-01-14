244acfe6e3a0d503d176ea6e539ee4d1e52f0a25
Ana Santoandre <anasantoandre84@gmail.com>
2023-01-14 12:28:19 -0300

    cambie de conexion la base

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 module.exports = {
-  "development": {
+  "test": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
@@ -8,7 +8,7 @@ module.exports = {
     "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
-  "test": {
+  "development": {
     "username": "root",
     "password": process.env.BD_LOCAL_PASSWORD,
     "database": "companiaalgodonera",
