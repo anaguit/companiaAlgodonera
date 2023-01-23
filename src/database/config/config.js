3fd2c066b9006566bd470f3b1bb796277c4c8979
Ana Santoandre <anasantoandre84@gmail.com>
2023-01-23 19:01:21 -0300

    deshice ultimos cambios

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -21,6 +21,12 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_PORT,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "dialectOptions":{
+      "ssl":{
+        "require":true,
+        "rejectUnauthorized":false
+      }
+    }
   }
 }
